ff80a35636f9c20469d15a8eb464b52ad43b6409
Juanma Yeboyebo <juanma.yeboyebo@gmail.com>
2020-01-24 14:38:16 +0100

    gitapilogin

--- motor/YBUTILS/APIQSA.py
+++ motor/YBUTILS/APIQSA.py
@@ -11,3 +11,8 @@ class APIQSA:
         # obj = qsa.from_project("formAPI").hola()
         obj = qsa.from_project("formAPI").entry_point(metodoHTTP, modulo, username, params, accion)
         return obj
+
+    def login(username, password):
+        # obj = qsa.from_project("formAPI").hola()
+        obj = qsa.from_project("formAPI").login(username, password)
+        return obj
