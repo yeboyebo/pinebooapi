74cbf588f894adb5e1f865451cab8afafa27022b
Antonio Rodriguez <antonio@yeboyebo.es>
2020-03-30 20:14:43 +0200

    Nueva variable de entorno WEBHOST para pruebas con IP pública

--- app/AQNEXT/local.py
+++ app/AQNEXT/local.py
@@ -1,10 +1,11 @@
 import os
 
-ALLOWED_HOSTS = (
-    'localhost',
-    '127.0.0.1',
-    'dbhost'
-)
+allowed = ['localhost', '127.0.0.1', 'dbhost']
+
+if os.environ.get('WEBHOST') is not None:
+    allowed.append(os.environ.get('WEBHOST'))
+
+ALLOWED_HOSTS = tuple(allowed)
 
 DATABASES = {
     'default': {
@@ -20,7 +21,8 @@ DATABASES = {
 
 StaticLoader = True
 dirs = []
-if os.environ.get('STATIC_LOADER_DIRS') != '':
+dir_list = os.environ.get('STATIC_LOADER_DIRS')
+if dir_list is not None and dir_list != '':
     for pdir in os.environ.get('STATIC_LOADER_DIRS').split(','):
         dirs.append(True)
         dirs.append(pdir)
