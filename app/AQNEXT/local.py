4e193f0acbf92d95544a98fc22244ab02fbe4549
José A. Fernández <aullasistemas@gmail.com>
2023-02-15 10:22:13 +0100

    La carga estática se desactiva automáticamente si no se espeicifa valor en el .env

--- app/AQNEXT/local.py
+++ app/AQNEXT/local.py
@@ -1,52 +1,54 @@
 import os
 
-allowed = ['localhost', '127.0.0.1', 'dbhost']
+allowed = ["localhost", "127.0.0.1", "dbhost"]
 
-if os.environ.get('WEBHOST') is not None:
-    allowed.append(os.environ.get('WEBHOST'))
+if os.environ.get("WEBHOST") is not None:
+    allowed.append(os.environ.get("WEBHOST"))
 
 ALLOWED_HOSTS = tuple(allowed)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DBNAME'),
-        'USER': os.environ.get('DBUSER'),
-        'PASSWORD': os.environ.get('DBPASSWORD'),
-        'HOST': 'dbhost',
-        'PORT': os.environ.get('DBPOST'),
-        'ATOMIC_REQUESTS': False
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DBNAME"),
+        "USER": os.environ.get("DBUSER"),
+        "PASSWORD": os.environ.get("DBPASSWORD"),
+        "HOST": "dbhost",
+        "PORT": os.environ.get("DBPOST"),
+        "ATOMIC_REQUESTS": False,
     }
 }
 
-websocket = os.environ.get('WEBSOCKET') or False
-USE_ATOMIC_LIST = os.environ.get('USE_ATOMIC_LIST') or False
+websocket = os.environ.get("WEBSOCKET") or False
+USE_ATOMIC_LIST = os.environ.get("USE_ATOMIC_LIST") or False
 StaticLoader = True
 dirs = []
-dir_list = os.environ.get('STATIC_LOADER_DIRS')
-if dir_list is not None and dir_list != '':
-    for pdir in os.environ.get('STATIC_LOADER_DIRS').split(','):
+dir_list = os.environ.get("STATIC_LOADER_DIRS")
+if dir_list is not None and dir_list != "":
+    for pdir in os.environ.get("STATIC_LOADER_DIRS").split(","):
         dirs.append(True)
         dirs.append(pdir)
+else:
+    StaticLoader = False
 
-temp_dir = os.environ.get('TEMPDIR')
+temp_dir = os.environ.get("TEMPDIR")
 
 if websocket == "True":
     CHANNEL_LAYERS = {
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                'hosts': [('dbhost', 6379)],
+                "hosts": [("dbhost", 6379)],
             },
         },
     }
 
     # Celery settings
-    BROKER_URL = 'redis://dbhost:6379/0'  # our redis address
+    BROKER_URL = "redis://dbhost:6379/0"  # our redis address
     # use json format for everything
-    CELERY_ACCEPT_CONTENT = ['json']
-    CELERY_TASK_SERIALIZER = 'json'
-    CELERY_RESULT_SERIALIZER = 'json'
+    CELERY_ACCEPT_CONTENT = ["json"]
+    CELERY_TASK_SERIALIZER = "json"
+    CELERY_RESULT_SERIALIZER = "json"
     ASGI_APPLICATION = "AQNEXT.routing.application"
 else:
-    WSGI_APPLICATION = 'AQNEXT.wsgi.application'
+    WSGI_APPLICATION = "AQNEXT.wsgi.application"
